3f351853bb6f887ce68bbd153b6b935b3542ce16
Abe Pazos <abe@hamoid.com>
2012-09-12 16:00:06 +0300

    Añado comentario sobre cómo usar db_sample.php

--- db_sample.php
+++ db_sample.php
@@ -1,3 +1,8 @@
 <?php
 
+  // No modifiques este archivo.
+  // Duplícalo. La nueva copia debe llamarse db.php
+  // Introduce los datos de tu base de datos en db.php
+  // (db.php será ignorado por Git)
+
   $my = new mysqli("localhost", "votaciones", "*********", "votaciones");
