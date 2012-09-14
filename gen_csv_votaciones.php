f842446ba584cfd0e50287db0b6fe7073e0d43ba
Abe Pazos <abe@hamoid.com>
2012-09-14 17:15:45 +0300

    Nuevas columnas en votaciones.csv

--- gen_csv_votaciones.php
+++ gen_csv_votaciones.php
@@ -9,11 +9,11 @@
 
   include_once('db.php');
 
-  $my->real_query("SELECT id, xml FROM votacion");
+  $my->real_query("SELECT id, xml, num_expediente FROM votacion");
   $res = $my->use_result();
 
   $fp = fopen($filename, 'w');
-  $bytes = fputcsv($fp, array('fecha', 'presentes', 'si', 'no', 'abs'));
+  $bytes = fputcsv($fp, array('fecha', 'presentes', 'si', 'no', 'abs', 'num_expediente', 'titulo', 'texto_expediente', 'titulo_subgrupo', 'texto_subgrupo'));
   while($row = $res->fetch_assoc()) {
     $r = new SimpleXMLElement($row['xml']);
 
@@ -24,7 +24,12 @@
         $r->Totales->Presentes,
         $r->Totales->AFavor,
         $r->Totales->EnContra,
-        $r->Totales->Abstenciones
+        $r->Totales->Abstenciones,
+        $row['num_expediente'],
+        $r->Informacion->Titulo,
+        $r->Informacion->TextoExpediente,
+        $r->Informacion->TituloSubGrupo,
+        $r->Informacion->TextoSubGrupo
       ));
     }
   }
