c20e14a4ca9cdc363c55a66b458f8d07271be05c
Abe Pazos <abe@hamoid.com>
2013-03-05 01:22:31 +0000

    Add iniciativa, votacion. Rename collection vars

--- qhdb.js
+++ qhdb.js
@@ -1,7 +1,11 @@
 var client = require('mongodb').MongoClient,
     ObjectID = require('mongodb').ObjectID,
-    pendingURL,
-    session;
+    async = require('async');
+
+var cPendingURL,
+    cSession,
+    cIniciativa,
+    cVotacion;
 
 var cfgMongoURL = 'mongodb://localhost:27017/votaciones',
     cfgStartURL = 'http://www.congreso.es/portal/page/portal/Congreso/Congreso/Actualidad/Votaciones';
@@ -14,9 +18,13 @@ exports.connect = function(cb) {
 			console.log("Error connecting to db: " + err);
 			process.exit(code=1);
 		}
+        // TODO: EnsureIndex (create indexes to avoid duplicated entries).
+        // Read todo.txt
 		console.log("Connected to db.");
-		pendingURL = db.collection("pendingURL");
-		session = db.collection("session");
+		cPendingURL = db.collection("pendingURL");
+		cSession    = db.collection("session");
+        cIniciativa = db.collection("iniciativa"); 
+        cVotacion   = db.collection("votacion");
 		cb();
 	});
 }
@@ -24,46 +32,74 @@ exports.connect = function(cb) {
 // Collection: pendingURL
 
 exports.cleanPendingURL = function(cb) {
-	pendingURL.remove(null, {w:1}, cb);
+	cPendingURL.remove(null, {w:1}, cb);
 }
 exports.insertPendingURL = function(url, cb) {
 	if(url == null)	url = cfgStartURL; 
-	pendingURL.insert({ url: url }, {w:1}, cb);
+	cPendingURL.insert({ url: url }, {w:1}, cb);
 }
 exports.getPendingURL = function(cb) {
-    pendingURL.findAndRemove({}, cb);
+    cPendingURL.findAndRemove({}, cb);
 }
 
 // Collection: session
 
 exports.insertIntoSession = function(data, cb) {
-	session.findOne({ fecha: data.fecha}, function(err, item) {
+	cSession.findOne({ fecha: data.fecha}, function(err, item) {
 		if(item)
 			cb(null, false);
 		else
-			session.insert({ fecha: data.fecha, url: data.url }, {w:1}, cb);
+			cSession.insert({ fecha: data.fecha, url: data.url }, {w:1}, cb);
 	});
 }
 exports.setSessionHtml = function(id, html, cb) {
-	session.update({ _id: id }, {$set:{html: html}}, {w:1}, cb);
+	cSession.update({ _id: id }, {$set:{html: html}}, {w:1}, cb);
+}
+exports.setSessionNum = function(id, num, cb) {
+    cSession.update({ _id: id }, {$set:{num: num}}, {w:1}, cb);
 }
 exports.getSessionWithNoHtml = function(cb) {
-    session.findOne({ html:null }, cb);
+    cSession.findOne({ html:null }, cb);
 }
 exports.getSessionWithNoNum = function(cb) {
-    session.findOne({ num:null}, cb);
+    cSession.findOne({ num:null}, cb);
 }
 
 // Collection: iniciativa
 
+exports.insertIniciativa = function(numExp, url, cb) {
+	cIniciativa.insert({ nupExpediente: numExp, url: url }, {w:1}, cb);
+}
+
 // Collection: votacion
+
+exports.insertVotacion = function(url, legis, num, numExp, cb) {
+	cVotacion.insert({ numExpediente: numExp, url:url, legislatura:legis, num:num }, {w:1}, cb);
+}
+
+// Info
+
+function ccount(collection, name) {
+        return function(cb) { collection.count(function(err, c) { console.log("  " + name + ":", c); cb(err); }) };
+}
+exports.info = function() {
+    console.log("db.votaciones.[collection].count()");
+    async.series([
+        ccount(cPendingURL, 'pendingURL'),
+        ccount(cSession, 'session'),
+        ccount(cIniciativa, 'iniciativa'),
+        ccount(cVotacion, 'votacion')
+    ], function(err, result) {
+        console.log(err ? err : 'Done.');
+    });
+}
 
 // Test
 
 exports.test = function() {
-	session.findOne({}, function(err, item) {
+	cSession.findOne({}, function(err, item) {
 		console.log(item);
-		pendingURL.findOne({}, function(err, item) {
+		cPendingURL.findOne({}, function(err, item) {
 			console.log(item);
 			process.exit(code=0);
 		});
