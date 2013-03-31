2e758b36f3aa1b4566d952dbe9fca6b92d2a7818
Abe Pazos <abe@hamoid.com>
2013-03-31 16:10:20 +0000

    Fix typos caused by global search/replace

--- tarea/gen_csv_votaciones.js
+++ tarea/gen_csv_votaciones.js
@@ -10,8 +10,8 @@
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WAres.ANTY; without even the implied warranty of
-    MEres.HANTABILITY or FITNESS FOres.A PAres.ICULAres.PUres.OSE.  See the
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
